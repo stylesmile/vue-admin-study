50850d6b5ecd6a4b9615bbe73208a61d96602c91
chenye <15002770045@139.com>
2020-03-28 20:48:23 +0800

    login

---
+++ vue-demo2-login/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Router)
+Vue.use(ElementUI)
+
+export default new Router({
+  routes: [
+    {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: () => import('@/components/login.vue')
+    }
+  ]
+})
